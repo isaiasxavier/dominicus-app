ba5b55b58c310f42dc76f43bde62758ca7b588d3
Isaias Xavier Santana <isaiasxavier@outlook.com>
2024-03-21 21:35:11 +0100

    Filament-Plug-in-SHIELD

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -31,7 +31,7 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
-                //
+
             ])
             ->filters([
                 //
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -20,6 +20,7 @@ use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 class AdminPanelProvider extends PanelProvider
 {
+
     public function panel(Panel $panel): Panel
     {
         return $panel
@@ -53,6 +54,9 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make()
             ]);
     }
 }
